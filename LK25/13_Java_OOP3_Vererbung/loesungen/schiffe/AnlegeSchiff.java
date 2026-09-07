493383172baf6b0e4049864775f5bb78b139934c
Hartmut Meyer <h.meyer6@schule.bremen.de>
2026-09-07 13:35:23 +0200

    .

--- LK25/13_Java_OOP3_Vererbung/loesungen/schiffe/AnlegeSchiff.java
+++ LK25/13_Java_OOP3_Vererbung/loesungen/schiffe/AnlegeSchiff.java
@@ -4,9 +4,10 @@ import java.awt.Graphics;
 public class AnlegeSchiff extends FensterSchiff {
 	private int stegPos;
 	private int anlegeZaehler = 0;
-	private final static int NICHT_ANGELEGT = 0;
+	private final static int NOCH_NICHT_ANGELEGT = 0;
 	private final static int ANGELEGT = 1;
-	private int zustand = NICHT_ANGELEGT;
+	private final static int BEREITS_ANGELEGT = 2;
+	private int zustand = NOCH_NICHT_ANGELEGT;
 
 	public AnlegeSchiff(Color f, int y, int speed, int stegPos) {
 		super(f, y, speed);
@@ -14,21 +15,31 @@ public class AnlegeSchiff extends FensterSchiff {
 	}
 
 	@Override
-	public void bewegen() {
+	public void bewegen() {	
 		switch (zustand) {
-			case NICHT_ANGELEGT -> {
+			case NOCH_NICHT_ANGELEGT -> {
 				super.bewegen();
-				if (xPos >= stegPos && xPos < stegPos + speed) {
+				if (xPos >= stegPos) {
 					zustand = ANGELEGT;
+					System.out.println("ANGELEGT");
 				}
 			}
 			case ANGELEGT -> {
 				anlegeZaehler++;
 				if (anlegeZaehler == 30) {
-					zustand = NICHT_ANGELEGT;
+					zustand = BEREITS_ANGELEGT;
+					System.out.println("BEREITS_ANGELEGT");
 					anlegeZaehler = 0;
 				}
 			}
+			case BEREITS_ANGELEGT -> {
+				super.bewegen();
+				if (xPos >= 500) {
+					zustand = NOCH_NICHT_ANGELEGT;
+					System.out.println("NOCH_NICHT_ANGELEGT");
+				}
+			}
 		}
+		
 	}
 }
